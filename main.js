1878c7d8d83e696b92b728504321201ded22ccfd
safoan bazine <safibazine@gmail.com>
2023-08-18 12:42:57 +0000

    added more css and js

---
+++ main.js
@@ -0,0 +1,11 @@
+function imgSlider(anything){
+    document.querySelector('.Starbucks') .src = anything;
+}
+
+function changecirclecolor(color){
+    const shape = document.querySelector('.circle');
+    shape.color;
+
+}
+
+
